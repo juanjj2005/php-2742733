382f7ad6f475f2d1316a02d1ed659423655c1629
Isa-0501 <isapalaciom8@gmail.com>
2024-05-10 16:34:50 -0500

    feat: sintaxis-variables

---
+++ 02-condicionales/index.php
@@ -0,0 +1,53 @@
+<?php
+
+$caja = '20';
+
+if ($caja <= 12){
+    echo 'LA CAJA ES RAPIDA';
+}
+
+else if($caja > 12){
+    echo 'LA CAJA ES LENTA';
+} 
+
+
+
+$edad = '10';
+
+if ($edad >= 18){
+    echo ' SI PUEDE VOTAR PAPI';
+}
+
+else if($edad < 18){
+    echo ' NO PUEDE VOTAR MIJO';
+}
+
+
+
+
+$nombre = 'carlos, mario';
+
+if ($nombre == 'Carlos, Mario'){
+    echo 'el usuario puede entrar al vip' ;
+}
+
+else if ($nombre <> 'Carlos, Mario'){
+    echo 'el usuario no puede entrar al vip' ;
+}
+
+$años = '18';
+
+if ($años >= '18'){
+    echo 'el  usuario puede ingresar';
+}
+
+else if ($años < '18'){
+    echo 'el usuario no puede ingresar';
+}
+
+
+
+
+echo 'EEEEEEEEEEEEEE'
+
+?>
